1ab02065cf562e9843d70b589a40eaa2c5140a19
Brigitte Mae Ibeng <115964613+Pearlesscucumber08@users.noreply.github.com>
2023-10-11 19:21:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstName = "Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB7");
+
+let password = 'p@ssw0rd';
+if ( password.length >= 8 &&  password.includes("p@ss")){
+        console.log("Valid password");
+} else{
+    console.log("Invalid password");
+}
